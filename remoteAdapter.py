57e658c6c21263d74a14ec4ccde1c5ddae2b74ea
Rezoan <rlikhon@gmail.com>
2022-03-30 18:57:58 +0600

    update readme

--- remoteAdapter.py
+++ remoteAdapter.py
@@ -12,14 +12,7 @@ from lightstreamer_adapter.interfaces.metadata import (MetadataProvider,
 from lightstreamer_adapter.interfaces.data import DataProvider, SubscribeError
 from lightstreamer_adapter.server import (DataProviderServer,
                                           MetadataProviderServer)
-__author__ = "Lightstreamer Srl"
-__copyright__ = "Copyright"
-__credits__ = ["Lightstreamer Srl"]
-__license__ = "Apache Licence 2.0"
-__version__ = "1.0.0"
-__maintainer__ = "Lightstreamer Srl"
-__email__ = "support@lightstreamer.com"
-__status__ = "Production"
+
 
 # The name of the item to subscribe to.
 ITEM_NAME = "chat_room"
